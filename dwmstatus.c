1b2138cc5e6940196d4bbb9bfe927ce4eb6d5306
Michael Cardell Widerkrantz <mc@hack.org>
2019-04-20 12:45:26 +0200

    Set X root's name.
    Eternal loop. Sleeping 10 seconds between updates.

--- dwmstatus.c
+++ dwmstatus.c
@@ -1,7 +1,12 @@
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/sysctl.h>
 #include <sys/time.h>
 #include <stdio.h>
+#include <xcb/xcb.h>
+#include <xcb/xcb_icccm.h>
+#include <string.h>
 
 int main(void) {
     int bat;
@@ -10,45 +15,77 @@ int main(void) {
     size_t len;
     int temp;
     char *batstring;
+    xcb_connection_t *conn;
+    xcb_screen_t *screen;
+    char status[80];
+    int scrno;
+    xcb_screen_iterator_t iter;
+
+    conn = xcb_connect(NULL, &scrno);
+    if (!conn)
+    {
+        fprintf(stderr, "can't connect to an X server\n");
+        exit(1);
+    }
+
+    iter = xcb_setup_roots_iterator(xcb_get_setup(conn));
+
+    for (int i = 0; i < scrno; ++i)
+    {
+        xcb_screen_next(&iter);
+    }
+
+    screen = iter.data;
 
     len = 4;
-    err = sysctlbyname("hw.acpi.battery.life", &bat, &len, NULL, 0);
-    if (err != 0) {
-        perror("sysctl");
-    }
 
-    err = sysctlbyname("hw.acpi.battery.state", &batstate, &len, NULL, 0);
-    if (err != 0) {
-        perror("sysctl");
-    }
-
-    switch (batstate)
+    while (1)
     {
-    case 0:
-        break;
-    case 1:
-        batstring = "B";
-        break;
-    case 2:
-        batstring = "AC";
-        break;
-    default:
-        batstring = "U";
-        break;
+        err = sysctlbyname("hw.acpi.battery.life", &bat, &len, NULL, 0);
+        if (err != 0)
+        {
+            perror("sysctl");
+        }
+
+        err = sysctlbyname("hw.acpi.battery.state", &batstate, &len, NULL, 0);
+        if (err != 0)
+        {
+            perror("sysctl");
+        }
+
+        switch (batstate)
+        {
+        case 1:
+            batstring = "B";
+            break;
+        case 2:
+            batstring = "AC";
+            break;
+        default:
+            batstring = "U";
+            break;
+        }
+
+        err = sysctlbyname("dev.cpu.0.temperature", &temp, &len, NULL, 0);
+        if (err != 0) {
+            perror("sysctl");
+        }
+
+        temp = (temp - 2732) / 10;
+
+        time_t rawtime;
+        struct tm *timeinfo;
+        time(&rawtime);
+        timeinfo = localtime(&rawtime);
+        char timestr[17];
+        strftime((char *)timestr, 17, "%F %R", timeinfo);
+        snprintf(status, 80, "%s%d%% | %dC | %s", batstring, bat, temp, timestr);
+
+        xcb_change_property(conn, XCB_PROP_MODE_REPLACE, screen->root, XCB_ATOM_WM_NAME,
+                            XCB_ATOM_STRING, 8, strlen(status), status);
+
+        xcb_flush(conn);
+
+        sleep(10);
     }
-
-    err = sysctlbyname("dev.cpu.0.temperature", &temp, &len, NULL, 0);
-    if (err != 0) {
-        perror("sysctl");
-    }
-
-    temp = (temp - 2732) / 10;
-
-    time_t rawtime;
-    struct tm *timeinfo;
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
-    char timestr[17];
-    strftime((char *)timestr, 17, "%F %R", timeinfo);
-    printf("%s%d%% | %dC | %s\n", batstring, bat, temp, timestr);
 }
